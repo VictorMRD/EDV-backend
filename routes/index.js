8024db669e4a551580a18af198393fed84d97821
VictorMRD <victormrd2002@hotmail.com>
2025-04-12 15:19:21 -0700

    Containerize the application
    Using nodemon --legacy-watch to check for changes in the container

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ping', (req, res) => {
+  res.send('pong 🏓 v2');
+});
+
 module.exports = router;
