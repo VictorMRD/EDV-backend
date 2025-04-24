5418ca968f388fcf0647d13972de190196a7c437
VictorMRD <victormrd2002@hotmail.com>
2025-04-24 16:14:42 -0700

    Add users routes to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* get all users from table */
+router.get('/', async (req, res) => {
+  try {
+    const usuarios = await User.find();
+    res.send(usuarios);
+  } catch (err) {
+    res.status(500).send({ error: 'Failed to fetch users' });
+  }
+});
+
+/* create an user. */
+router.post('/create', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send({ error: 'Error at saving the user information' });
+  }
 });
 
 module.exports = router;
